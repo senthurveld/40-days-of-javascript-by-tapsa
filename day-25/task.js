c6711a0d4005c4984eebef375ecc808fe014324f
senthurveld <150390304+senthurveld@users.noreply.github.com.>
2025-12-23 21:38:50 +0530

    day 24

---
+++ day-25/task.js
@@ -0,0 +1,163 @@
+// retrieve a list of users
+
+// async function users() {
+//   try {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+
+//     if (!res.ok) {
+//       throw new Error("Failed");
+//     }
+//     const data = await res.json();
+
+//     data.forEach((elem) => {
+//       console.log(elem.name);
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+// users();
+
+// Fetch all posts by userId=1
+
+// async function users() {
+//   try {
+//     const res = await fetch(
+//       `https://jsonplaceholder.typicode.com/posts?userId=1`
+//     );
+
+//     if (!res.ok) {
+//       throw new Error("Failed");
+//     }
+//     const data = await res.json();
+
+//     data.forEach((elem) => {
+//         let p = document.createElement("p");
+//         p.innerHTML = `<p>${elem.title}<p/>`;
+//         console.log(elem.title);
+//     });
+
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+// users();
+
+// async function postMethod(postData) {
+//   const api = `https://jsonplaceholder.typicode.com/posts`;
+
+//   try {
+//     const res = await fetch(api, {
+//       method : "POST",
+//       headers : {"Content-type" : "application/json"},
+//       body : JSON.stringify(postData)
+//     });
+//     const data = await res.json();
+//     console.log(res);
+//   }
+//   catch(error) {
+//     console.error(error);
+//   }
+// }
+
+// const newData = {
+//   "title" : "dei thambi thanga kamni",
+//   "body" : "ommala dei yarra",
+//   "userId" : 9
+// }
+// postMethod(newData);
+
+
+// async function putMethod(postData) {
+//   const api = `https://jsonplaceholder.typicode.com/posts/1`;
+
+//   try {
+//     const res = await fetch(api, {
+//       method: "PUT",
+//       headers: { "Content-type": "application/json" },
+//       body: JSON.stringify(postData),
+//     });
+//     const data = await res.json();
+//     console.log(res);
+//     console.log(data);
+    
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// const newData = {
+//   title: "vankam",
+//   body: "wanna cuma",
+// };
+// putMethod(newData);
+
+
+// async function patchMethod(postData) {
+//   const api = `https://jsonplaceholder.typicode.com/posts/1`;
+
+//   try {
+//     const res = await fetch(api, {
+//       method: "PUT",
+//       headers: { "Content-type": "application/json" },
+//       body: JSON.stringify(postData),
+//     });
+//     const data = await res.json();
+//     console.log(res);
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// const newData = {
+//   title: "darling ku dampaku",
+// };
+// patchMethod(newData);
+
+
+
+// async function delMethod(id) {
+//   const api = `https://jsonplaceholder.typicode.com/posts/${id}`;
+
+//   try {
+//     const res = await fetch(api, {
+//       method: "DELETE"
+//     });
+//     const data = await res.json();
+//     console.log(res);
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+// delMethod(34);
+
+
+
+async function ctLog(postData) {
+  const api = `https://jsonplaceholder.typicode.com/posts`;
+
+  try {
+    const res = await fetch(api, {
+      method : "POST",
+      headers : {"Content-type" : "application/json"},
+      body : JSON.stringify(postData)
+    });
+    const data = await res.json();
+    console.log(res);
+    console.log(data);
+    
+  }
+  catch(error) {
+    console.error(error);
+  }
+}
+
+const newData = {
+  "title" : "dei thambi thanga kamni",
+  "body" : "ommala dei yarra",
+  "userId" : 9
+}
+ctLog(newData);
